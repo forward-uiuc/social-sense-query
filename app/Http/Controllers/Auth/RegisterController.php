f86fc5417187a8a09349619d4418b6a1475d5820
Dustyn Tubbs <DustynTubbs@Gmail.com>
2018-04-23 09:14:58 -0500

    Fix the path for getting an application

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
-use App\Application;
+use App\Models\Application;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
